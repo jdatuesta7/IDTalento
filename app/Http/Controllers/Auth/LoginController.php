226df5ede98f2f2da265e1f532469333bbf3c216
jdatuesta7 <jdatuesta7@gmail.com>
2021-05-03 14:29:35 -0500

    a

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,7 +40,7 @@ class LoginController extends Controller
     }
 
     /**
-     * Redirect the user to the GitHub authentication page.
+     * Redirect the user to the Red social actual authentication page.
      *
      * @return \Illuminate\Http\Response
      */
@@ -50,14 +50,14 @@ class LoginController extends Controller
     }
 
     /**
-     * Obtain the user information from GitHub.
+     * Obtain the user information from Red social actual.
      *
      * @return \Illuminate\Http\Response
      */
     public function handleProviderCallback($driver)
     {
         $user = Socialite::driver($driver)->user();
-
+        dd($user);
         // $user->token;
     }
 }
